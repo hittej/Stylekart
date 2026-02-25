ddaf4eedf7d7ae40f6144d07b651b6a68e95d4da
HITTEJ RAJPUROHITH <68469041+hittej@users.noreply.github.com>
2026-02-25 22:18:31 +0530

    Update Firebase configuration with new project details

--- firebase/config.js
+++ firebase/config.js
@@ -1,13 +1,14 @@
 
 // Replace with your Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
-};
+    apiKey: "AIzaSyBPfvV9uw5i_TQjONqXdL__ZPpidSdnVAU",
+    authDomain: "stylekart-26639.firebaseapp.com",
+    projectId: "stylekart-26639",
+    storageBucket: "stylekart-26639.firebasestorage.app",
+    messagingSenderId: "752821082150",
+    appId: "1:752821082150:web:a73c7f5113c9b5c7a4aee7",
+    measurementId: "G-ESQ2LDCRG0"
+  };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
